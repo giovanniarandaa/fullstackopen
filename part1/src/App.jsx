dbed312845ed418eadeeb17d64d2a38475091f8f
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-10-24 11:09:36 -0600

    1.6: unicafe, paso 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,33 +1,22 @@
-import Content from "./Content";
-import Header from "./Header";
-import Total from "./Total";
+import { useState } from "react";
 
 function App() {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h2>Give feedback</h2>
+      <button type="button" onClick={() => setGood(p => p + 1)}>good</button>
+      <button type="button" onClick={() => setNeutral(p => p + 1)}>neutral</button>
+      <button type="button" onClick={() => setBad(p => p + 1)}>bad</button>
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
-  );
+  )
 }
 
 export default App;
