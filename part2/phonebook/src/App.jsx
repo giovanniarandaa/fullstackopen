d13cf5690dc130433d98350ae629c91d96513a12
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-10-28 23:53:43 -0600

    2.8: La Agenda Telefónica Paso 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,49 +1,68 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
-    ])
-    const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-1234567" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        const existsName = persons.find((person) => person.name === newName)
+    const existsName = persons.find((person) => person.name === newName);
 
-        if (existsName) {
-            alert(`${newName} is already added to phonebook`)
-            return;
-        }
-
-        setPersons(p => ([
-            ...p,
-            {
-                name: newName
-            }
-        ]))
-        setNewName('')
+    if (existsName) {
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
 
-    return (
-        <div>
-            <h2>Phonebook</h2>
-            <form>
-                <div>
-                    name: <input type="text" value={newName} onChange={e => setNewName(e.target.value)} />
-                </div>
-                <div>
-                    <button type="submit" onClick={handleSubmit}>add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            <ul>
-                {persons.map((person, index) => (
-                    <li key={index}>{person.name}</li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+    setPersons((p) => [
+      ...p,
+      {
+        name: newName,
+        phone: newPhone,
+      },
+    ]);
+    setNewName("");
+  };
 
-export default App
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name:{" "}
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            type="tel"
+            value={newPhone}
+            onChange={(e) => setNewPhone(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmit}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>
+            {person.name} {person.phone}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
