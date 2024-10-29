77fa45ee388a58f74726eeaabb1e7d46a25cda91
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-10-28 23:37:01 -0600

    2.7: La Agenda Telefónica Paso 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,6 +6,25 @@ const App = () => {
     ])
     const [newName, setNewName] = useState('')
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const existsName = persons.find((person) => person.name === newName)
+
+        if (existsName) {
+            alert(`${newName} is already added to phonebook`)
+            return;
+        }
+
+        setPersons(p => ([
+            ...p,
+            {
+                name: newName
+            }
+        ]))
+        setNewName('')
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
@@ -14,11 +33,15 @@ const App = () => {
                     name: <input type="text" value={newName} onChange={e => setNewName(e.target.value)} />
                 </div>
                 <div>
-                    <button type="submit">add</button>
+                    <button type="submit" onClick={handleSubmit}>add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            ...
+            <ul>
+                {persons.map((person, index) => (
+                    <li key={index}>{person.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
