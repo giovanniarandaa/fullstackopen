f6a537474e7a8fca419200cf737955e4b986f41b
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-11-01 11:44:18 -0600

    3.4: Backend de la Agenda Telefónica, paso 4

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -38,6 +38,12 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
     }
 
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+    response.status(204).end()
 })
 
 app.get('/info', (request, response) => {
