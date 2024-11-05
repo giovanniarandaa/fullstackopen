d76ba24909442ea13e76f9b554389d7040a9d5cb
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-11-04 20:02:11 -0600

    4.5*: Funciones Auxiliares y Pruebas Unitarias, paso 3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs = []) => {
   return blogs.reduce((acc, current) => acc + current.likes, 0);
 };
 
+const favoriteBlog = (blogs = []) => {
+  if (blogs.length === 0) return null;
+  return blogs.reduce((acc, current) =>
+    acc.likes < current.likes ? current : acc,
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
