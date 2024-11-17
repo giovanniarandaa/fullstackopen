8ccb66154afd8853987d8a2cd2e95271807399eb
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-11-17 13:31:57 -0600

    exercises 4.15 & 4.16*

---
+++ part4/controllers/users.js
@@ -0,0 +1,31 @@
+const usersRouter = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const { username, name, password } = req.body;
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
+
+    const savedUser = await user.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get("/", async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+module.exports = usersRouter;
--- part4/tests/api/test_helper.js
+++ part4/tests/api/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require("../../models/blog");
+const User = require("../../models/user");
 
 const initialBlogs = [
   {
@@ -20,6 +21,11 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON());
 };
 
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((user) => user.toJSON());
+};
+
 const nonExistingId = () => {
   return "673529e59b286eeebd55ca21";
 };
@@ -28,4 +34,5 @@ module.exports = {
   initialBlogs,
   blogsInDb,
   nonExistingId,
+  usersInDb,
 };
