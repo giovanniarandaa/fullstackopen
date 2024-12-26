b85394bbd3716f64c046571d86c26d6feae29a7e
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-12-25 20:13:14 -0600

    5.16: Pruebas de Listas de Blogs, paso 4

--- part5/src/components/BlogForm.test.jsx
+++ part5/src/components/BlogForm.test.jsx
@@ -17,3 +17,29 @@ test("<BlogForm /> updates parent state and calls onSubmit", async () => {
   expect(createBlog).toHaveBeenCalledTimes(1);
   expect(createBlog.mock.calls[0][0].title).toBe("testing a form...");
 });
+
+test("calls onSubmit with correct details when a new blog is created", async () => {
+  const mockAddBlog = vi.fn();
+  const user = userEvent.setup();
+
+  render(<BlogForm addBlog={mockAddBlog} />);
+
+  const titleInput = screen.getByRole("textbox", { name: "title" });
+  const authorInput = screen.getByRole("textbox", { name: "author" });
+  const urlInput = screen.getByRole("textbox", { name: "url" });
+  const createButton = screen.getByRole("button", { name: "create" });
+
+  await user.type(titleInput, "Testing Blog Title");
+  await user.type(authorInput, "Testing Blog Author");
+  await user.type(urlInput, "http://testblog.com");
+
+  await user.click(createButton);
+
+  expect(mockAddBlog).toHaveBeenCalledTimes(1);
+
+  expect(mockAddBlog.mock.calls[0][0]).toEqual({
+    title: "Testing Blog Title",
+    author: "Testing Blog Author",
+    url: "http://testblog.com",
+  });
+});
