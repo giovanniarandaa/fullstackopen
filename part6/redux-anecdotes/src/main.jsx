531f4530ee76aec2ca76b75a6280a1a3d34c7c42
Giovanni Aranda <giovanni.arandaa@gmail.com>
2025-01-06 18:08:06 -0600

    6.3: Anécdotas, paso 1

---
+++ part6/redux-anecdotes/src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import reducer from "./reducers/anecdoteReducer.js";
+
+const store = createStore(reducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
